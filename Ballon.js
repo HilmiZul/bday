08bb4d18f082d69d04f21a260852408e4bbf6cbf
hilmizul <netspytux@gmail.com>
2018-12-25 10:28:25 +0700

    first commit

---
+++ Ballon.js
@@ -0,0 +1,49 @@
+class Ballon {
+  constructor() {
+    this.r = random(90, 150);
+    this.pos = createVector(random(width), random(height, height + random(300, 700)));
+    this.upStep = 5;
+    this.red = random(100, 255);
+    this.green = random(100, 255);
+    this.blue = random(100, 255);
+    this.lineLen = random(150, 250);
+  }
+
+  show() {
+    // line
+    push();
+    stroke(255);
+    strokeWeight(2);
+    line(this.pos.x, this.pos.y + this.r / 2 + 10, this.pos.x, this.pos.y + this.lineLen);
+    pop();
+
+    push();
+    // balloon
+    fill(this.red, this.green, this.blue, 170);
+    noStroke();
+    ellipse(this.pos.x, this.pos.y, this.r, this.r + 10);
+    // balloon tie
+    ellipse(this.pos.x, this.pos.y + this.r / 2 + 10, 10, 7);
+    pop();
+  }
+
+  up() {
+    this.pos.y -= this.upStep;
+  }
+
+  checkEdge() {
+    if (this.pos.y < 0 + this.r / 2 + 5) {
+      this.pos.y = 0 + this.r / 2 + 5;
+    }
+  }
+
+  mouseHover() {
+    if (this.pos.x + this.r / 2 > mouseX && this.pos.x - this.r / 2 < mouseX) {
+      if (this.pos.y + this.r / 2 > mouseY && this.pos.y - this.r / 2 < mouseY) {
+        return true;
+      }
+    } else {
+      return false;
+    }
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,82 @@
+/*
+ * b'day card
+ * created: 21 December 2018
+ * d-day: 24 December 🎉
+ * 
+ * https://hilmizul.github.io/bday
+ */
+
+var mobils = [];
+var balloons = [];
+var total = 10;
+var font;
+var foto, bgStatic;
+var txt = "HAPPY B'DAY";
+var name = "TO ME";
+var sfxPop;
+
+function preload() {
+	foto = loadImage("assets/img/foto.png");
+	bgStatic = loadImage("assets/img/jakarta.png");
+	font = loadFont("assets/font/FredokaOne.ttf");
+	sfxPop = loadSound("assets/sfx/pop.mp3");
+}
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+
+	for (let i = 0; i < total; i++) {
+		balloons.push(new Ballon());
+	}
+}
+
+function draw() {
+	background(114, 51, 153);
+	
+	image(bgStatic, 0, 0, width, height);
+
+	push();
+	var imgX = width / 2;
+	var imgY = height / 2;
+	// foto
+	translate(imgX + (-mouseX + width / 2) / 30, imgY + (-mouseY + height / 2) / 30);
+	imageMode(CENTER);
+	image(foto, 0, 0, 200, 200);
+	// frame
+	noFill();
+	stroke(250, 150);
+	strokeWeight(10);
+	ellipse(0, 0, 210, 210);
+	pop();
+
+	// name
+	push();
+	fill(255);
+	stroke(45, 28, 82);
+	strokeWeight(7);
+	textFont(font);
+	textSize(50);
+	textAlign(CENTER);
+	// name
+	text(name, imgX + (mouseX - width / 2) / 30, imgY + 200 + (mouseY - height / 2) / 30);
+	// teks hbd
+	text(txt, imgX + (mouseX - width / 2) / 30, imgY - 150 + (mouseY - height / 2) / 30);
+	pop();
+
+	// balloons
+	for (let i = 0; i < balloons.length; i++) {
+		balloons[i].show();
+		balloons[i].up();
+		balloons[i].checkEdge();
+		if (balloons[i].mouseHover()) {
+			sfxPop.play();
+			balloons.splice(i, 1);
+		}
+	}
+
+	if (balloons.length < 3) {
+		for (let i = 0; i < total; i++) {
+			balloons.push(new Ballon());
+		}
+	}
+}
